2fb296e414e1cb0e7f317025131915ee727a833b
veenay910 <veenay910@gmail.com>
2025-09-24 15:59:03 +0530

    o4-variable

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,10 +2,10 @@
 
 # Passing arguments
 
-$PERSON1=$1
-$PERSON2=$2
+PERSON1=$1
+PERSON2=$2
 
-echo "$1: Hi $2! You look worried. What’s wrong?
+echo "$1: Hi $2! You look worried. What’s wrong?"
 
 echo "$2: Hi $1! I am stressed about the upcoming exams. There’s so much to study!"
 
@@ -19,6 +19,4 @@ echo "$2: That’s interesting! What about Science?"
 
 echo "$1: Use diagrams and real-life examples. Science is everywhere! For example, Newton’s Law of Motion explains how cars move."
 
-echo "$2: That makes sense! Let’s study together and revise important topics."
-
-echo "$1: Great idea! We’ll help each other. Let’s start today."
+echo "$2: That makes sense! Lets study together and revise important topics"
