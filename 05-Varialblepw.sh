bcce8f15db917ae58dd558b68c401670e0534736
veenay910 <veenay910@gmail.com>
2025-09-24 17:37:44 +0530

    05-PW

---
+++ 05-Varialblepw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter the PIN number"
+
+read -s PIN_NUMBER  # PIN_NUMBER is a variable 
+
+echo "the entered pin number is $PIN_NUMBER"
+
+echo "enter the pw"
+
+read PW
+
+echo "enter pw is $PW"
+
+# read will dispaly the password
+# read -s hide the password
