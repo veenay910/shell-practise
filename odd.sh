45e50302e14a952852149164f557490df941463e
veenay910 <veenay910@gmail.com>
2025-09-26 18:10:22 +0530

    test

---
+++ odd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#odd number 
+
+echo "Enter the value :"
+
+read=number
+
+if [$(($number % 2)) -gt 0]; then
+    echo "given value is odd number"
+
+else   
+    echo "given value is even number"
+
+fi
---
+++ prime.sh
@@ -0,0 +1,9 @@
+#!/bin.bash
+
+#script for the prime number conidtion
+#prime number = one and itself dividable
+
+
+read number
+
+if [$(($number ))]
