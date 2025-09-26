b4813d8537775617b6343ef11d593fecdd9ee4b6
veenay910 <veenay910@gmail.com>
2025-09-26 11:54:07 +0530

    test

--- 01.Hellow-worls.sh
+++ 01.Hellow-worls.sh
@@ -3,4 +3,11 @@
 echo "Hellow world"
 echo "Hellow world"
 
+# echo command display the context what entered on it
+
+echo "waht is shebang?"
+
+echo "The shebang is special line at the very begining of the script that tells the operating system which interpter
+to use executing the script"
+
 
