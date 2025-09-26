e2ea731d1092486ec95c75fff7fc511ebd265990
veenay910 <veenay910@gmail.com>
2025-09-26 21:05:30 +0530

    test

---
+++ 12-installation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#scipt to install the mysql server
+
+# 1. Need to run with sudo user
+# 2. if not stop the installation
+# 3. if yes install the mysql 
+# 4. 
+#
+#
+
+USERID=$(id -u)
+
+if ($USERID -ne 0); then
+    echo "ERROR:: script should be run using sudo permissions"
+
+else
+    echo "Script is running through sudo permissions"
+
+#dnf install mysql-server -y
+#systemctl enable mysqld
+#systemctl start mysqld  
