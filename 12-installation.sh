eb80b28c0747dd3bda62ca7824935b0994a08aae
veenay910 <veenay910@gmail.com>
2025-09-27 19:54:26 +0530

    12

--- 12-installation.sh
+++ 12-installation.sh
@@ -5,14 +5,14 @@
 # 1. Need to run with sudo user
 # 2. if not stop the installation
 # 3. if yes install the mysql 
-# 4. 
+# 4. #? exit status of previous command 0=success, otherwise failed
 #
 #
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "Not running with sudo permissions"
+    echo "ERROR:: Please run the script wit sudo permissions"
 fi
 
 
