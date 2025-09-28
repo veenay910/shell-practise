28731e23573ec36df7d2cee8f003eda0eb4b9519
veenay910 <veenay910@gmail.com>
2025-09-28 14:53:44 +0530

    12

--- 13-Fucntions.sh
+++ 13-Fucntions.sh
@@ -5,4 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Error: Script should be run with sudo previlages"
 fi
-   
+
+dnf install nginx -y
+
+if [ $? -eq 0 ]; then
+    echo" Nginx installed successfully"
+fi
