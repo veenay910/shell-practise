a3f58a4acbe141eb795ef9ebb6c9dd97a534a3a5
veenay910 <veenay910@gmail.com>
2025-09-28 17:12:04 +0530

    12

--- 14.colors.sh
+++ 14.colors.sh
@@ -5,11 +5,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+AGE=$age
 
 echo -e "What is your name $R vinay $N"
 
 
-
-echo -e "What is your age $G $age $N"
-
-read age
+echo -e "What is your age $G $AGE $N"
