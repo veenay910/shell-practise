e80ff2c886e77406c324ec33feef22d0d7cc6163
veenay910 <veenay910@gmail.com>
2025-09-28 22:30:07 +0530

    12

--- 15-colors.sh
+++ 15-colors.sh
@@ -1,9 +1,13 @@
 #!/bin.bash
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $? -ne 0 ]; then
-    echo "ERROR: Should run only with sudo privilages"
+    echo -e "$R ERROR: $N Should run only with sudo privilages"
     exit 1
 else
     echo "Sudo permission verified proceeding"
@@ -11,10 +15,10 @@ fi
 
 dnf list installed nginx
 if [ $? -ne 0 ]; then
-    echo "Nginx not installed......proceedin to install nginx"
+    echo "Nginx not installed......$G proceedin to install nginx $N"
 dnf install nginx -y
 else
-    echo "nginx already installed --- skipping"
+    echo "nginx already installed --- $Y skipping $N"
     echo "Already installed on "
 dnf history list nginx | head -3 | tail -1 | awk '{print $7}'
 fi
