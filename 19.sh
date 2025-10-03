4cf08aa671c8a87715a8f3e6e110ce7004cef891
veenay910 <veenay910@gmail.com>
2025-10-03 22:01:51 +0530

    12

--- 19.sh
+++ 19.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
+#!/bin/bahs
 
-SCRIPT_NAME=$( echo $0 )
-
-echo " $SCRIPT_NAME "
-
-
-mkdir -p LOG_FOLDER "/var/log/shellscript"
-touch 19.log /var/log/shellscript
-if [ $? -ne 0 ]; then
-    echo " Created folder"
-else    
-    echo "Folder alredy exist"
-fi
-
-#create log file
-LOG_FOLDER="/var/log/shellscript"
-Script_file=$( echo $0 )
-echo "$Script_file"
-LOG_FILE=$LOG_FOLDER/$Script_file.log
-
-dnf list installed nginx &>>LOG_FILE
+mkdir -p "var/log/test"
