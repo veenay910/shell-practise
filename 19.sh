c6b522a3a3109c6a3c63815599c838b72a6a9b66
veenay910 <veenay910@gmail.com>
2025-10-03 21:43:59 +0530

    12

--- 19.sh
+++ 19.sh
@@ -5,5 +5,9 @@ SCRIPT_NAME=$( echo $0 )
 echo " $SCRIPT_NAME "
 
 
-mkdir LOG_FOLDER "/var/log/shellscript"
+mkdir -p LOG_FOLDER "/var/log/shellscript"
+if [ $? -ne 0 ]; then
+    echo " Created folder"
+fi
+
 
