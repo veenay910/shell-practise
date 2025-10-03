346970a6a7caa2e505150b77df9874508192b2c3
veenay910 <veenay910@gmail.com>
2025-10-03 17:16:54 +0530

    12

--- 18-loops.sh
+++ 18-loops.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 echo "print $USERID "
+#color code for shell script
+R='\033[0;31m'
+G='\033[0;32m'
+B='\033[0;34m'
+N='\033[0m'
+
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    echo "Ngnix not exit"
+    dnf install nginx -y
+else 
+    echo -e "Ngnix already installed ...$B skipping $N "
+
+fi
+
+
 
