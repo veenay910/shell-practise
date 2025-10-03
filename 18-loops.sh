f23caf808c7b14d553f9a92023f4d63e6a9490ce
veenay910 <veenay910@gmail.com>
2025-10-03 18:04:24 +0530

    12

--- 18-loops.sh
+++ 18-loops.sh
@@ -8,6 +8,10 @@ G='\033[0;32m'
 B='\033[0;34m'
 N='\033[0m'
 
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE=$0 
+
+
 dnf list installed nginx
 if [ $? -ne 0 ]; then
     echo -e "Ngnix not exit ...Proceeding to install $G Nginx $N "
